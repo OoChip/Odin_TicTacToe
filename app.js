540adf8f8c78d2c0c56a4358530b7d86408efb42
OoChip <oochip2001@gmail.com>
2022-03-07 17:18:22 -0500

    playable

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const d = document
 
 //Objeto gameBoard para controlar el estado del tablero.
-const gameBoard = {
-    square1:0, square2:0, square3:0,
-    square4:0, square5:0, square6:0,
-    square7:0, square8:0, square9:0
+let gameBoard = {
+    1:0, 2:0, 3:0,
+    4:0, 5:0, 6:0,
+    7:0, 8:0, 9:0
 }
 
 const playerA = {
@@ -47,8 +47,8 @@ function handlerSwitchStamp(e){
 
 function game(Event){
     //Deshabilitar boton Switch y habilita boton Reset.
-    btnSwitch.style.display = "none"
-    btnReset.style.display = "initial";
+    btnSwitch.style.visibility = "hidden"
+    btnReset.style.visibility = "visible"
 
     //identifica la casilla clickeada.
     squarePlayed = d.getElementById(`${Event.target.id}`);
@@ -68,43 +68,56 @@ function game(Event){
         //Guarda la jugada en el objeto gameBoard
         gameBoard[squarePlayed.getAttribute("id")] = lastPlayer.rol
 
-    
-
-
         //llama funcion que verifica si hay un ganador
         evaluateWinner()
     }
 }
+
 
 function evaluateWinner(){
     const winnerPatters = [
         [1,2,3], [4,5,6], [7,8,9],
         [1,4,7], [2,5,8], [3,6,9],
         [1,5,9], [3,5,7]]   
+         
+    const played = Number(squarePlayed.id)
+    let acum = 0
+
+        //Busca la casilla Jugada en los patrones ganadores.
+        winnerPatters.forEach(function(element){
+            if(element.includes(played)){           // Filtra los patrones ganadores en los que esta presente la casilla jugada.
+                element.forEach(function(element){  // Evalua cada casilla de las lineas, de cada patron ganador filtrado.
+                    if (gameBoard[element] === 0){  //si alguna de las casilla de la linea en evaluacion no ha sido jugada (vale 0 en el tablero) no hay ganador.
+                        return
+                    }else{                                                        
+                        if (gameBoard[element] === "x" ){ acum++ }else { acum--} // cada x suma 1, cada O resta 1, al final solo si acum vale 4 o -4 singifica que huvo un ganador
+               }
+            })
+        console.log(acum);
+        }
+
+    if(acum === 4 || acum === -4 ){
+        acum = 0
+        endGame()
+    }
+    })
     
-    // Formatea el Id de la casilla Jugada para buscarla en los patrones ganadores.
-    const played = squarePlayed.getAttribute("id").slice(6)
-
-    // console.log("evaluando ganador");
-    // console.log( played); 
-
-    //Busca la casilla Jugada en los patrones ganadores.
-
-    //verifica si en los patrones ganadores que contienen la casilla jugada todas las jugadas son iguales a X u O.
-
-    //habemus winner.
-    winner = lastPlayer
-    // console.log(`Gano ${lastPlayer.player}. ${lastPlayer.rol}`);
-
-    //funcion que finaliza el juego, resetea gameBoard y el tablerohtml
-    endGame()
 }
 
 //funcion que finaliza el juego, resetea gameBoard y el tablerohtml
 function endGame(){
     //Deshabilitar boton Reset y habilita boton Switch.
-    btnSwitch.style.display = "initial"
-    btnReset.style.display = "none";
+    btnSwitch.style.visibility = "visible"
+    btnReset.style.visibility = "hidden"
+   alert(`Fin del Juego. Gano ${lastPlayer.rol}`);
+
+   //Reiniciar valores
+   squares.forEach ( element => element.setAttribute ("src", "./assets/img/empy.svg"))
+
+   gameBoard = {
+    1:0, 2:0, 3:0,
+    4:0, 5:0, 6:0,
+    7:0, 8:0, 9:0
 }
 
-
+}
